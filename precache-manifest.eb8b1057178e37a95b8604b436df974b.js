f195c819a72af8c65b169b19291190ef025a4263
Vladimir <bobuzh@gmail.com>
2020-05-26 21:03:27 +0300

    Updates

---
+++ precache-manifest.eb8b1057178e37a95b8604b436df974b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fefca8fbd56425481ad93e92f3cf205",
+    "url": "/my-easy-trello/index.html"
+  },
+  {
+    "revision": "b39470f45870e9d0c7d2",
+    "url": "/my-easy-trello/static/css/main.e4dac078.chunk.css"
+  },
+  {
+    "revision": "912c4e0c5dcb0eb3de26",
+    "url": "/my-easy-trello/static/js/2.501c3579.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-easy-trello/static/js/2.501c3579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39470f45870e9d0c7d2",
+    "url": "/my-easy-trello/static/js/main.35e682e0.chunk.js"
+  },
+  {
+    "revision": "1333cb60171cdac44530",
+    "url": "/my-easy-trello/static/js/runtime-main.10899ea3.js"
+  }
+]);
