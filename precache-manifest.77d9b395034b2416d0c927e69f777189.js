6138bf8be3b322baf8541584893173c829bc2e2a
Vladimir <bobuzh@gmail.com>
2020-05-27 15:15:22 +0300

    Updates

--- precache-manifest.77d9b395034b2416d0c927e69f777189.js
+++ precache-manifest.77d9b395034b2416d0c927e69f777189.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d642408741158247442d1505cce8124",
+    "revision": "876cd4a3614daaf41f52fb069c502d1f",
     "url": "/my-easy-trello/index.html"
   },
   {
-    "revision": "f3f535e4d5f065d5dd6b",
+    "revision": "e9eccdf599c4dc0bd144",
     "url": "/my-easy-trello/static/css/main.e4dac078.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-easy-trello/static/js/2.9f4e05bc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f3f535e4d5f065d5dd6b",
-    "url": "/my-easy-trello/static/js/main.07744eb6.chunk.js"
+    "revision": "e9eccdf599c4dc0bd144",
+    "url": "/my-easy-trello/static/js/main.d81dc186.chunk.js"
   },
   {
     "revision": "1333cb60171cdac44530",
