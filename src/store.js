00c261f21740d9d4be03fd198031e12057bc1285
Vladimir <bobuzh@gmail.com>
2020-05-27 11:04:25 +0300

    ...

--- src/store.js
+++ src/store.js
@@ -1,13 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 import thunk from 'redux-thunk';
-import { compose } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const composeEnhancers = process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null
-    || compose;
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware( thunk )));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware( thunk )));
 
 export default store;
